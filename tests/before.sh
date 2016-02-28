d555730e36c3d4ea3e0ddeb5cd6a15c846df16e7
Brady Owens <info@fastglass.net>
2016-02-28 09:07:04 +0100

    Changed variables to camelCase and added tests to delete nodes and confirm tracking code was disabled.

--- tests/before.sh
+++ tests/before.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-pwd
 cd $DRUPAL_TI_DRUPAL_DIR
-pwd
 wget https://www.drupal.org/files/issues/drupal-session_destroy_return_bool-2460833-30.patch
 git apply drupal-session_destroy_return_bool-2460833-30.patch
