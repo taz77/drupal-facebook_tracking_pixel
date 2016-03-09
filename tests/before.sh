a425367de9de8cc9c07e446e915fed3e44efb10e
Brady Owens <info@fastglass.net>
2016-03-09 21:00:41 +0100

    Enable Commerce via Drush.

--- tests/before.sh
+++ tests/before.sh
@@ -4,3 +4,4 @@ cd $DRUPAL_TI_DRUPAL_DIR
 wget https://www.drupal.org/files/issues/drupal-session_destroy_return_bool-2460833-30.patch
 git apply drupal-session_destroy_return_bool-2460833-30.patch
 drush dl commerce
+drush en -y commerce_tax, commerce_price, commerce_cart, commerce_product, commerce_order, commerce_store, commerce
