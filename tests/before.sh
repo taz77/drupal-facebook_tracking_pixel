601d62383eee8d4dda56df8a7004935289622527
Brady Owens <info@fastglass.net>
2016-03-09 20:36:20 +0100

    Allow testing runners to enable commerce.

--- tests/before.sh
+++ tests/before.sh
@@ -4,4 +4,3 @@ cd $DRUPAL_TI_DRUPAL_DIR
 wget https://www.drupal.org/files/issues/drupal-session_destroy_return_bool-2460833-30.patch
 git apply drupal-session_destroy_return_bool-2460833-30.patch
 drush dl commerce
-drush en -y commerce
