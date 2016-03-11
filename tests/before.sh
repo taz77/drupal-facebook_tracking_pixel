76518bce3a6513f7e957f866874ab8e9b9928fee
Brady Owens <info@fastglass.net>
2016-03-11 07:22:35 +0100

    Download the dev version of Ctools with PHP7 fix.

--- tests/before.sh
+++ tests/before.sh
@@ -3,5 +3,6 @@
 cd $DRUPAL_TI_DRUPAL_DIR
 wget https://www.drupal.org/files/issues/drupal-session_destroy_return_bool-2460833-30.patch
 git apply drupal-session_destroy_return_bool-2460833-30.patch
+drush dl ctools-7.x-3.x-dev
 drush dl commerce-7.x-1.11
 drush en -y commerce_tax, commerce_price, commerce_cart, commerce_product, commerce_order, commerce
