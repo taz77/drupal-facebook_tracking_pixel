448e81222c3c76fdff26804cb40b028b46e70d6b
Brady Owens <info@fastglass.net>
2016-03-24 09:46:53 +0100

    Travis troubleshooting.

--- tests/before.sh
+++ tests/before.sh
@@ -3,6 +3,7 @@
 cd $DRUPAL_TI_DRUPAL_DIR
 wget https://www.drupal.org/files/issues/drupal-session_destroy_return_bool-2460833-30.patch
 git apply drupal-session_destroy_return_bool-2460833-30.patch
+pwd
 drush dl ctools-7.x-1.x-dev
 drush dl commerce-7.x-1.11
 drush en -y commerce_tax, commerce_price, commerce_cart, commerce_product, commerce_order, commerce
