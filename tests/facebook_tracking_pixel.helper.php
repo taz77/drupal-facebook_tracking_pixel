376b5db334a33987d44bca90415bfa7a929db24d
Brady Owens <info@fastglass.net>
2016-05-01 21:23:00 +0200

    Test work for the inverse role function.

--- tests/facebook_tracking_pixel.helper.php
+++ tests/facebook_tracking_pixel.helper.php
@@ -8,11 +8,29 @@
 namespace facebookTrackingPixel;
 
 class FacebookTrackingPixelTestHelper {
+
+  /**
+   * Enable tracking for all roles.
+   */
   public function enable_tracking_all_roles() {
     // Turn on tracking for roles.
     variable_set('facebook_tracking_pixel_roles_administrator', 1);
     variable_set('facebook_tracking_pixel_roles_anonymous_user', 1);
     variable_set('facebook_tracking_pixel_roles_authenticated_user', 1);
   }
+
+  /**
+   * Enable tracking for only the testing role.
+   */
+
+  public function enable_tracking_testing_role(){
+    // Inverse the visibility.
+    variable_set('facebook_tracking_pixel_visibility_roles', 1);
+    variable_set('facebook_tracking_pixel_roles_fb_pixel_tester', 1);
+    variable_set('facebook_tracking_pixel_roles_administrator', 0);
+    variable_set('facebook_tracking_pixel_roles_anonymous_user', 0);
+    variable_set('facebook_tracking_pixel_roles_authenticated_user', 0);
+  }
+
 }
 
