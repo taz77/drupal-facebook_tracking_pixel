d49eacd788ce248f269d2f21159408fdb5b41bba
Brady Owens <info@fastglass.net>
2016-05-02 00:43:42 +0200

    Incremental commit.

--- tests/facebook_tracking_pixel.helper.php
+++ tests/facebook_tracking_pixel.helper.php
@@ -32,5 +32,37 @@ class FacebookTrackingPixelTestHelper {
     variable_set('facebook_tracking_pixel_roles_authenticated_user', 0);
   }
 
+  /**
+   * Remove a role from a user.
+   *
+   * @param $user
+   *   User object or user ID.
+   * @param $role_name
+   *   String value of role to be removed.
+   */
+  public function remove_role_from_user($user, $role_name) {
+    // For convenience, we'll allow user ids as well as full user objects.
+    if (is_numeric($user)) {
+      $user = user_load($user);
+    }
+    // Only remove the role if the user already has it.
+    $key = array_search($role_name, $user->roles);
+    if ($key == TRUE) {
+      // Get the rid from the roles table.
+      $roles = user_roles(TRUE);
+      $rid = array_search($role_name, $roles);
+      if ($rid != FALSE) {
+        // Make a copy of the roles array, without the deleted one.
+        $new_roles = array();
+        foreach($user->roles as $id => $name) {
+          if ($id != $rid) {
+            $new_roles[$id] = $name;
+          }
+        }
+        user_save($user, array('roles' => $new_roles));
+      }
+    }
+  }
+
 }
 
